3fdd25d4cb5fdf74a3e72949c54ca45775b5c59e
josecarlos47 <jcfj270504@gmail.com>
2023-05-19 10:32:47 +0200

    finalizado

--- htmlUsuario/javas.js
+++ htmlUsuario/javas.js
@@ -1,20 +1,22 @@
 function validarCampos() {
     let campo1 = document.getElementById("password").value;
-    let campo2 = document.getElementById("rpassword").value;
+    let campo2 = document.getElementById("rpassword").value; 
     
+    let error = document.getElementById("error");
+
     if (campo1 !== campo2) {
-      alert("Los campos deben ser iguales");
+
+      error.style.display = 'inline-block'
+
+      return event.preventDefault()
+
       
-      return false;
     }else{
-        
-
-        return true;
-
+      return document.getElementById("registro").action = 'indice.html';
     }
     
-    
   }
+
 
  function ocultarDiv(btn) {
       var div = btn.parentElement;
